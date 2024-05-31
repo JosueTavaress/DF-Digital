d0badf8e98ea7092c14bc1a8247a05c71fa3ec6c
JosueTavaress <josueferreira8.jf@gmail.com>
2024-05-30 23:36:48 -0300

    swagger (refactor): add underline separator

--- backend/src/swagger.ts
+++ backend/src/swagger.ts
@@ -22,7 +22,7 @@ function swaggerDocs(app: Express) {
   app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
   // Docs in JSON format
-  app.get("/docs.json", (req: Request, res: Response) => {
+  app.get("/docs.json", (_req: Request, res: Response) => {
     res.setHeader("Content-Type", "application/json");
     res.send(swaggerSpec);
   });
