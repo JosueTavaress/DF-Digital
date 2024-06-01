72a9cd3fb90b37ae36d55d0c89bdf42554227184
JosueTavaress <josueferreira8.jf@gmail.com>
2024-06-01 11:54:03 -0300

    validation (feat): add body validator

--- backend/src/validator/index.ts
+++ backend/src/validator/index.ts
@@ -1,5 +1,7 @@
 import userBodyValidator from "./user-body-validator";
+import authBodyValidator from "./auth-body-validator";
 
 export default [
-  userBodyValidator
+  userBodyValidator,
+  authBodyValidator
 ]
