02ef4091a2b4ea58560ee968a4daa68d3c6fe0a8
JosueTavaress <josueferreira8.jf@gmail.com>
2024-06-01 11:51:08 -0300

    user (refactor): refactor structure response client

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -4,15 +4,15 @@ import { ErrorService } from '../services/interface';
 
 const getAllUsers = async (_req: Request, res: Response) => {
   const response = await getAll();
-  return res.send(response);
+  return res.status(200).json(response);
 };
 
 const createUser = async (req: Request, res: Response) => {
   try {
     const userId = await create(req.body);
-    return res.status(201).send({ id: userId, ...req.body });
+    return res.status(201).json({ id: userId, ...req.body });
   } catch (err: ErrorService & any) {
-    return res.status(err.code).send(err.message);
+    return res.status(err.code).json({message: err.message});
   }
 }
 
