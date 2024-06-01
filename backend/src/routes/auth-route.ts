02b00e0f4e6892c330e887b305f03c8d2f703900
JosueTavaress <josueferreira8.jf@gmail.com>
2024-06-01 13:47:28 -0300

    auth (doc): create doc auth login

--- backend/src/routes/auth-route.ts
+++ backend/src/routes/auth-route.ts
@@ -4,5 +4,70 @@ import bodyValidator from '../middlewares/body-validator';
 
 export const authRouter = Router();
 
+/**
+ * @openapi
+ * '/auth':
+ *   post:
+ *     tags:
+ *     - Auth
+ *     summary: Login
+ *     requestBody:
+ *       description: Get token
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: Email of the user
+ *               password:
+ *                 type: string
+ *                 description: Password of the user
+ *             required:
+ *               - email
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Token created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 tempToken:
+ *                   type: string
+ *                   description: Unique identifier for the user
+ *                 expiresAt:
+ *                   type: string
+ *                   description: Name of the user
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   code:
+ *                     type: string
+ *                     example: invalid_type
+ *                   expected:
+ *                     type: string
+ *                     example: string
+ *                   received:
+ *                     type: string
+ *                     example: undefined
+ *                   path:
+ *                     type: array
+ *                     items:
+ *                       type: string
+ *                     example: ["email"]
+ *                   message:
+ *                     type: string
+ *                     example: Required
+ */
 authRouter.post('/', [bodyValidator, login]);
 
