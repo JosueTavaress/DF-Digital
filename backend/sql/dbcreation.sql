e6af1b730941119f2f0679586f5921ffb9fc6e2a
JosueTavaress <josueferreira8.jf@gmail.com>
2024-06-02 00:58:14 -0300

    db (feat): create relation in user > tag

--- backend/sql/dbcreation.sql
+++ backend/sql/dbcreation.sql
@@ -35,18 +35,44 @@ CREATE TABLE IF NOT EXISTS `dfDigital`.`tag` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `description` VARCHAR(255) NOT NULL,
+  `color` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
+-- -----------------------------------------------------
+-- Table `dfDigital`.`user_tag`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dfDigital`.`user_tag` (
+  `user_id` INT NOT NULL,
+  `tag_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `tag_id`),
+  CONSTRAINT `fk_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `dfDigital`.`user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_tag`
+    FOREIGN KEY (`tag_id`)
+    REFERENCES `dfDigital`.`tag` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
-INSERT INTO `dfDigital`.`tag` (`name`, `description`) VALUES ('primeiro-contato', 'Etapa onde é feito o primeiro atendimento do cliente');
-INSERT INTO `dfDigital`.`tag` (`name`, `description`) VALUES ('reunião', 'Etapa de interesse do cliente, reunião para alinhamento');
-INSERT INTO `dfDigital`.`tag` (`name`, `description`) VALUES ('suporte', 'Etapa de auxílio ao cliente');
-INSERT INTO `dfDigital`.`tag` (`name`, `description`) VALUES ('finalizado', 'Prestação concluída');
+INSERT INTO `dfDigital`.`tag` (`name`, `description`, `color`) VALUES ('primeiro-contato', 'Etapa onde é feito o primeiro atendimento do cliente', '#FF5733');
+INSERT INTO `dfDigital`.`tag` (`name`, `description`, `color`) VALUES ('reunião', 'Etapa de interesse do cliente, reunião para alinhamento', '#33FF57');
+INSERT INTO `dfDigital`.`tag` (`name`, `description`, `color`) VALUES ('suporte', 'Etapa de auxílio ao cliente', '#3357FF');
+INSERT INTO `dfDigital`.`tag` (`name`, `description`, `color`) VALUES ('finalizado', 'Prestação concluída', '#FF33A1');
 
 INSERT INTO `dfDigital`.`user` (`name`, `email`, `password`) VALUES ('João Ferreira', 'joaoferreira@gmail.com', '$2b$10$sHAZ.YUlJe28hkWz1R1VU.9Qf3TFnIhRWHLxGYK8ChFC6P4uVzhua');
 INSERT INTO `dfDigital`.`user` (`name`, `email`, `password`) VALUES ('Nilce Silva', 'nilcesilva@gmail.com', '$2b$10$QAnB..zw8WonV2SoBPg91uZzdIo3NiYsu44aptGL/MdXtMrFuvtai');
 INSERT INTO `dfDigital`.`user` (`name`, `email`, `password`) VALUES ('Pedro Nava', 'pedronava@gmail.com', '$2b$10$WjHpciTraYVP/l1PVZ0oGu6eY6lZWbOXuPKLkq8LB.7H1zQqweyYS');
+
+INSERT INTO `dfDigital`.`user_tag` (`user_id`, `tag_id`) VALUES
+((SELECT `id` FROM `dfDigital`.`user` WHERE `name` = 'João Ferreira'), (SELECT `id` FROM `dfDigital`.`tag` WHERE `name` = 'primeiro-contato')),
+((SELECT `id` FROM `dfDigital`.`user` WHERE `name` = 'João Ferreira'), (SELECT `id` FROM `dfDigital`.`tag` WHERE `name` = 'finalizado')),
+((SELECT `id` FROM `dfDigital`.`user` WHERE `name` = 'Nilce Silva'), (SELECT `id` FROM `dfDigital`.`tag` WHERE `name` = 'suporte')),
+((SELECT `id` FROM `dfDigital`.`user` WHERE `name` = 'Nilce Silva'), (SELECT `id` FROM `dfDigital`.`tag` WHERE `name` = 'reunião')),
+((SELECT `id` FROM `dfDigital`.`user` WHERE `name` = 'Pedro Nava'), (SELECT `id` FROM `dfDigital`.`tag` WHERE `name` = 'primeiro-contato'));
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
