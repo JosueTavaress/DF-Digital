64530c8ec4d49a45f32ec257310a4d72c0faa807
JosueTavaress <josueferreira8.jf@gmail.com>
2024-06-03 04:44:16 -0300

    http (feat): create requests login

--- frontend/src/api/http-request-interface.ts
+++ frontend/src/api/http-request-interface.ts
@@ -1,4 +1,23 @@
-export interface IUsersApi {
+export interface IResponseLogin {
+  expiresAt: string,
+  tempToken: string
+}
+
+export interface IResponseCreateUser {
+  id: number,
+  email: string,
+  password: string,
+  name: string
+}
+
+export interface IResponseUser {
   name: string,
-  email: string
+  email: string,
+  id: 1,
+  tags: {
+    id: number,
+    name: string,
+    description: string,
+    color: string
+  }[]
 }
--- frontend/src/api/http-request.ts
+++ frontend/src/api/http-request.ts
@@ -1,15 +1,49 @@
 import axios from "axios";
+import { IResponseLogin, IResponseCreateUser, IResponseUser } from './http-request-interface';
+import { getToken } from '../utils/storage-browser';
 
 const request = axios.create({
   baseURL: "http://localhost:4000"
 });
 
-const getUsers = async () => 
-  request({
+const login = async (email: string, password: string): Promise<IResponseLogin> => {
+  const response = await request({
+    method: "POST",
+    url: "/auth",
+    data: {
+      email,
+      password
+    }
+  });
+  return response.data;
+}
+
+const createUser = async (data: { email: string, password?: string, name: string }): Promise<IResponseCreateUser> => {
+  const response = await request({
+    method: "POST",
+    url: "/user",
+    data: {
+      email: data.email,
+      password: data.password,
+      name: data.name
+    }
+  });
+
+  return response.data;
+}
+
+const getUsers = async (): Promise<IResponseUser[]> => {
+  const token = getToken();
+  const response = await request({
     method: "GET",
-    url: "/user"
+    url: "/user",
+    headers: { Authorization: `Bearer ${token}` }
   })
+  return response.data;
+}
 
 export default {
-  getUsers
+  getUsers,
+  login,
+  createUser
 }
