0cdb2f70b885ce31502b82cdb88aceea9168f571
JosueTavaress <josueferreira8.jf@gmail.com>
2024-06-04 12:06:46 -0300

    test (fix): fix test create user

--- backend/src/test/E2E/users.test.ts
+++ backend/src/test/E2E/users.test.ts
@@ -3,9 +3,11 @@ import superTest from "supertest";
 import { app } from '../../app';
 
 describe("API Suite E2E Users", () => {
-  test("GET /user should return an array", async () => {
-    const response = await superTest(app).get('/user');
+  test("POST /user should return an array", async () => {
+    const response = await superTest(app).post('/user').send({ name: 'john2', email: "john2@gmail.com", password: "123" })
     const data = response.body;
-    expect(data).toBeInstanceOf(Array);
+    expect(data).toHaveProperty("id");
+    expect(data).toHaveProperty("name");
+    expect(data).toHaveProperty("email");
   });
 });
