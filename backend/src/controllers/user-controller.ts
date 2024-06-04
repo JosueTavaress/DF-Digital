b3ba4d8841ea80674291a30ceab36eebbb6a6ee3
JosueTavaress <josueferreira8.jf@gmail.com>
2024-06-04 05:39:34 -0300

    user (refactor): refactor domain errors in user

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,30 +1,45 @@
-import { Response, Request } from 'express';
+import { Response, Request, NextFunction } from 'express';
 import { getAll, create, update, deleteUser as modeDeleteUser } from '../services/user-service';
-import { ErrorService } from '../services/interface';
+import { ApiError, InternalServerError } from '../errors/errors-http';
 
-const getAllUsers = async (_req: Request, res: Response) => {
-  const response = await getAll();
-  return res.status(200).json(response);
+const getAllUsers = async (_req: Request, res: Response, next: NextFunction) => {
+  try {
+    const response = await getAll();
+    return res.status(response.statusCode).json(response.data);
+  } catch (_error) {
+    next(new InternalServerError());
+  }
 };
 
-const createUser = async (req: Request, res: Response) => {
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const userId = await create(req.body);
-    return res.status(201).json({ id: userId, ...req.body });
-  } catch (err: ErrorService & any) {
-    return res.status(err.code).json({ message: err.message });
+    const data = await create(req.body);
+    if (!data.isValidRequest) {
+      next(new ApiError(data.message!, data.statusCode))
+    }
+    return res.status(data.statusCode).json({ id: data, ...req.body });
+  } catch (_error) {
+    next(new InternalServerError());
   }
 }
 
-const updateUser = async (req: Request, res: Response) => {
-  const data = await update(Number(req.params.id), req.body);
-  res.status(200).json(data);
+const updateUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { data, statusCode } = await update(Number(req.params.id), req.body);
+    res.status(statusCode).json(data);
+  } catch (_error) {
+    next(new InternalServerError());
+  }
 }
 
-const deleteUser = async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  await modeDeleteUser(id);
-  res.status(200).json({ message: "user deleted" })
+const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = Number(req.params.id);
+    await modeDeleteUser(id);
+    res.status(200).json({ message: "user deleted" })
+  } catch (_error) {
+    next(new InternalServerError());
+  }
 }
 
 export {
--- backend/src/services/user-service.ts
+++ backend/src/services/user-service.ts
@@ -1,33 +1,52 @@
 import userModel from '../models/user-model/user-model';
 import { IUsers, IUserWithTags, IUpdateUser } from '../models/user-model/interface';
 import { hashPassword } from './utils/hash';
+import { HTTP_CODE } from '../errors/errors-http';
+import { IObjectResponse } from './utils/error-object';
 
-const getAll = async (): Promise<Omit<IUserWithTags, "password">[]> => {
+const getAll = async (): Promise<IObjectResponse<Omit<IUserWithTags, "password">[]>> => {
   const users = await userModel.getAllUsers();
-  return users.map((el) => {
-    return { name: el.name, email: el.email, id: el.id, tags: el.tags }
-  });
+  const objectData = {
+    isValidRequest: true,
+    statusCode: HTTP_CODE.HTTP_OK,
+    data: users.map((el) => {
+      return { name: el.name, email: el.email, id: el.id, tags: el.tags }
+    })
+  }
+  return objectData;
 }
 
-const create = async (user: Omit<IUsers, "id">): Promise<number> => {
+const create = async (user: Omit<IUsers, "id">): Promise<IObjectResponse<number>> => {
   const userExist = await userModel.getUserByEmail(user.email);
   if (userExist) {
-    const error = {
+    const objectError = {
+      isValidRequest: false,
       message: `O email ${user.email} já está registrado.`,
-      code: 409
+      statusCode: HTTP_CODE.HTTP_CONFLICT,
     }
-    throw error;
+    return objectError;
   }
   const password = user.password ? user.password : "default";
   const encodePassword = hashPassword(password);
   const newUserId = await userModel.createUser({ ...user, password: encodePassword });
-  return newUserId;
+  const objectData = {
+    isValidRequest: true,
+    statusCode: HTTP_CODE.HTTP_OK,
+    data: newUserId
+  }
+  return objectData;
 }
 
 const update = async (userId: number, data: IUpdateUser) => {
   const userLinksTag = [...new Set(data.user_links_tag)];
   data.user_links_tag = userLinksTag;
-  return userModel.updateUser(userId, data);
+  const updated = await userModel.updateUser(userId, data);
+  const objectResponse = {
+    isValidRequest: true,
+    statusCode: HTTP_CODE.HTTP_OK,
+    data: updated
+  }
+  return objectResponse;
 }
 
 const deleteUser = async (id: number): Promise<void> => {
